30772a682295bdc428e49e601e5f248337c4ce21
pcstoyle <pcstoyle@hotmail.com>
2023-11-22 12:23:27 -0700

    initial commit, added starter files

---
+++ models/index.js
@@ -0,0 +1,27 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.hasOne(Category, {
+  foreignKey: '',
+  onDelete: 'CASCADE'
+})
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: ','
+})
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
